d8795aedd5e565da127ec483b624b1f13bf0daa5
VirajDalave <virajdvd7@gmail.com>
2025-07-13 11:47:46 +0530

    Test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def index():
     message = ""
     if request.method == 'POST':
         name = request.form.get('name')
-        message = f"Hello, {name}!"
+        message = f"Hello, {name}! "
     return render_template('index.html', message=message)
 
 if __name__ == '__main__':
